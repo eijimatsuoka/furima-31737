23c0da32acc8250ebb26f70d88e7a3d533bb5cf8
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-11-19 12:43:15 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,13 @@ class User < ApplicationRecord
   validates :password, confirmation: true
   validates :nickname, presence: true
   validates :last_name, presence: true
-  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :first_name, presence: true
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :last_name_kana, presence: true
-  validates :last_name_kana, format: { with: /\p{katakana}/
-  /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :first_name_kana, presence: true
-  validates :first_name_kana, format: { with: /\p{katakana}/
-  /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birth_date, presence: true
-  validates_format_of :password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
+  validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
 end
