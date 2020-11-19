2655d88828a360599b52f729af4b37274f0d43c8
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-11-19 19:37:43 +0900

    画像の投稿

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :handling_time_id)
+    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :handling_time_id, :image).merge(user_id: current_user.id)
   end
 end
