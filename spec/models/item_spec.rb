53b34ce9d7cb9fa7b78111270232369466c91877
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-11-20 20:37:39 +0900

    販売手数料等の計算

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '' do
+    context '商品出品がうまくいくとき' do
+      it 'imageとuser、descriptionとcategory、conditionとpostage、prefectureとhandling_time、priceが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
