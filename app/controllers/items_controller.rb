ec25790bf2c8ecc70e45411a43dc2970d2581406
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-11-23 19:37:37 +0900

    コントローラーの修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,8 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id == @item.user_id
-      @item.destroy
+      @item.destroy 
+      redirect_to root_path
     else
       redirect_to root_path
     end
