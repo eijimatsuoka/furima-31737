69160f5829481f3ac427cc816d07d14915c2da6d
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-11-28 17:00:20 +0900

    決済機能の実装

--- app/controllers/purchaces_controller.rb
+++ app/controllers/purchaces_controller.rb
@@ -9,6 +9,7 @@ class PurchacesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchace_address = PurchaceAddress.new(purchace_params)
     if @purchace_address.valid?
+      pay_item
       @purchace_address.save
       redirect_to root_path
     else
@@ -18,7 +19,16 @@ class PurchacesController < ApplicationController
 
   private
    def purchace_params
-    params.require(:purchace_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchace_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_number, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
    end
+
+   def pay_item
+    Payjp.api_key = 'sk_test_85765af652c2d7da4a6bded4'
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchace_params[:token],
+      currency: 'jpy'
+    )
+  end
 
 end
